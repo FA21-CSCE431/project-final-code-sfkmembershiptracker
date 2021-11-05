bbae9ba3b36bf225ab714798f49fe20f7ff69845
Nicholas Anderson <nick.b.anderson4@gmail.com>
2021-11-04 19:46:23 -0500

    ran db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_25_074514) do
+ActiveRecord::Schema.define(version: 2021_11_05_004426) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "application_answers", force: :cascade do |t|
+    t.text "answer"
+    t.text "question"
+    t.string "member_email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "application_questions", force: :cascade do |t|
+    t.text "question"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "applications", force: :cascade do |t|
     t.string "first_name"

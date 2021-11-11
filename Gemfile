f8c635c0c70b7bde167043f4283bcf067aacdd1b
Steven Clark <55724740+spclark@users.noreply.github.com>
2021-11-10 20:38:02 -0600

    Revert "Nick L"

--- Gemfile
+++ Gemfile
@@ -66,6 +66,3 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'cssbundling-rails'
-
-gem 'irb'
-gem 'rb-readline'
--- app/controllers/_contact_form_controller.rb
+++ app/controllers/_contact_form_controller.rb
@@ -1,39 +1,13 @@
 class ContactFormController < ApplicationController
-  def index
-    #@contact_form = ContactForm.new()
-	@contact_forms = ContactForm.new()
-  end
-  
   def create
-    @contact_form = ContactForm.new()
-    @contact_form.name = params[:name]
-    @contact_form.email = params[:email]
-    @contact_form.message = params[:message]
-	respond_to do |format|
-	  if @contact_form.deliver
-		#render json: {message: "Email sent!"}
-	  
-		format.html { redirect_to '/about', notice: "Email was successfully sent." }
-		format.json { render :show, status: :created, location: '/about' }
-		#flash.now[:success] = 'Email was successfully sent.'
-	  else
-	    render json: @contact_form.errors
-	  end
+    @contact = Contact.new()
+    @contact.name = params[:name]
+    @contact.email = params[:email]
+    @contact.message = params[:message]
+    if @contact.deliver
+      render json: {message: "Email sent!"}
+    else
+      render json: @contact.errors
     end
   end
-  
-  #def new
-    #@contact = ContactForm.new
-  #end
-
-  #def create
-    #@contact = Contact.new()
-	#@contact = ContactForm.new(params[:contact_form])
-    #@contact.request = request
-    #if @contact.deliver
-      #render json: {message: "Email sent!"}
-    #else
-      #render json: @contact.errors
-    #end
-  #end
 end

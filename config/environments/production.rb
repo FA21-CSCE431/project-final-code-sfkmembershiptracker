6f2fbc610221ec0e7913d5f78a25b393d251afef
Steven Clark <spclark@users.noreply.github.com>
2021-11-11 01:59:00 -0600

    Fixed Nick's work that I screwed up

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,22 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'https://sfk-membership-tracker.herokuapp.com' }
   config.action_mailer.perform_caching = false
+  Rails.application.routes.default_url_options[:host] = 'https://sfk-membership-tracker.herokuapp.com'
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default charset: 'utf-8'
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com'
+    port: 587
+    domain: 'gmail.com'
+    authentication: 'plain'
+    enable_starttls_auto: true
+    #user_name: ENV['GMAIL_USERNAME']
+    #password: ENV['GMAIL_PASSWORD']
+	  user_name: 'sfk.website.431@gmail.com'
+    password: 'sfk431admin'
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.

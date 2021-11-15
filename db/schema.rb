8d328b6cffb86de20263b02366204f3ffa67dd19
kiddrock025 <kiddrock025@tamu.edu>
2021-11-15 14:16:39 -0600

    Fixed members issue, minor text changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_12_064425) do
+ActiveRecord::Schema.define(version: 2021_11_14_201538) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"

c088faedc66534a8172b3fc737a1915c144abd0e
kiddrock025 <kiddrock025@tamu.edu>
2021-11-15 21:42:23 -0600

    Unit testing for Announcements and Members, partial integration testing for Announcements

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -19,3 +19,45 @@ RSpec.describe 'Test OAuth', type: :feature do
     expect(page).to have_content('Status')
   end
 end
+
+RSpec.describe 'Test Announcements', type: :feature do  
+  scenario 'Valid inputs' do
+	# Need OAuth
+	visit new_blog_post_path
+	fill_in 'Name', with: 'John Doe'
+	fill_in 'Position', with: 'Officer'
+	fill_in 'Message', with: 'This is a test message.'
+	click_on 'Create Blog post'
+	visit blog_posts_path
+	expect(page).to have_content('John Doe')
+	expect(page).to have_content('Officer')
+	expect(page).to have_content('This is a test message.')
+  end
+  
+  scenario 'Only name was input' do
+    # Need OAuth
+	visit new_blog_post_path
+	fill_in 'Name', with: 'John Doe'
+	click_on 'Create Blog post'
+	visit blog_posts_path
+	expect(page).not_to have_content('John Doe',)
+  end
+  
+  scenario 'Only position was input' do
+    # Need OAuth
+	visit new_blog_post_path
+	fill_in 'Position', with: 'Member'
+	click_on 'Create Blog post'
+	visit blog_posts_path
+	expect(page).not_to have_content('Member',)
+  end
+  
+  scenario 'Only message was input' do
+    # Need OAuth
+	visit new_blog_post_path
+	fill_in 'Message', with: 'Another test message.'
+	click_on 'Create Blog post'
+	visit blog_posts_path
+	expect(page).not_to have_content('Another test message.',)
+  end
+end

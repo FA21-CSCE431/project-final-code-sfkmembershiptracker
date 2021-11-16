ac5d8b53beef46c6aaa7bda1cc2e1a821b331169
Sutrisna <43864788+trisroy@users.noreply.github.com>
2021-11-15 19:53:10 -0600

    created unit tests for events and sfkinfos entities

--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -29,3 +29,68 @@ RSpec.describe User, type: :model do
     expect(subject).to be_valid
   end
 end
+
+Rspec.describe Events do
+  subject do
+    described_class.new(name: 'Meeting 1', event_type: 'Meeting', date: '11-15-2021', location: 'MSC', points: 1, confirmation_code: 'test')
+  end
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without a name' do
+    subject.name = nil
+    expect(subject).not_to be valid
+  end
+
+  it 'is not valid without an event type' do
+    subject.event_type = nil
+    expect(subject).not_to be valid
+  end
+
+  it 'is not valid without a date' do
+    subject.date = nil
+    expect(subject).not_to be valid
+  end
+
+  it 'is not valid without an location' do
+    subject.location = nil
+    expect(subject).not_to be valid
+  end
+
+  it 'is not valid without points' do
+    subject.points = nil
+    expect(subject).not_to be valid
+  end
+
+  it 'is not valid without an confirmation code' do
+    subject.confirmation_code = nil
+    expect(subject).not_to be valid
+  end
+end
+
+Rspec.describe SfkInfos do
+  subject do
+    described_class.new(mission: 'This is our mission.', history: 'This is our history.', ig_link: 'testlink')
+  end
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without mission' do
+    subject.mission = nil
+    expect(subject).not_to be valid
+  end
+
+  it 'is not valid without history' do
+    subject.history = nil
+    expect(subject).not_to be valid
+  end
+
+  it 'is not valid without instagram link' do
+    subject.ig_link = nil
+    expect(subject).not_to be valid
+  end
+end

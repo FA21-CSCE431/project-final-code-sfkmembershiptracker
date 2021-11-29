0f235bbdfaf429bdbfa530a27d404bc196df8bac
Steven Clark <spclark@users.noreply.github.com>
2021-11-28 20:28:49 -0600

    Added asset path tags

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module TestApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
     config.exceptions_app = self.routes
+    config.serve_static_assets = true
 
     # Configuration for the application, engines, and railties goes here.
     #
